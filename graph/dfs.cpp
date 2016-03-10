47e0ab1494a98428a3600f479b32194d4ff4858f
Marlon Mendes <marlonacademy@hotmail.com>
2016-03-10 10:38:06 -0300

    Depth-first-search time technique

--- graph/dfs.cpp
+++ graph/dfs.cpp
@@ -11,20 +11,30 @@ using namespace std;
 
 // DFS - Depth-first-search
 // DFS is backtracking, but never calls the same state twice. It's used to traverse a graph (directed or not)
+/* This DFS keeps a global "time". 
+ * Every vertex has a first and a second time variables;
+ * First = first time this vertex is discovered in the dfs algorithm;
+ * Second = after dfs is runnad in each unvisited vertex in adjList of the current the vertex,
+ *          the second time is recorded;
+ * This technique is useful for more advanced algorithms;
+ */
 
 const int N = 1234;
 
 vector< vector<int> > adjList; // adjacency list representation.
-bool visited[N];
+int first[N], last[N];
+int total;
 
 void dfs(int u) {
-    visited[u] = true;
+    printf(" -> %d", u);
+
+    first[u] = ++total;
     for(int v : adjList[u]) {
-        if(!visited[v]) {
-            printf("%d -> %d\n", u, v);
+        if(first[v] == 0) {
             dfs(v);
         }
     }
+    last[u] = ++total;
 }
 
 int main() {
@@ -39,7 +49,17 @@ int main() {
     }
 
     int source = 1;
+
+    total = 0;
+    memset(first, 0, sizeof first);
+    memset(last, 0, sizeof last);
+
     dfs(source);
+    printf("\n");
+
+    for(int i = 1; i <= v; ++i) {
+        printf("(first, last, %d) = %d, %d\n", i, first[i], last[i]);
+    }
 
     return 0;
 }

a6d2974e5dc84f69a4d92c7b20db9553c0365fd7
Marlon Mendes <marlonacademy@hotmail.com>
2017-06-03 07:25:38 -0300

    template, vimrc, bashrc

---
+++ setup/.vimrc
@@ -0,0 +1,13 @@
+set number
+set tabstop=4 expandtab shiftwidth=4
+set autoindent
+set cindent
+syntax on
+set pastetoggle=<F2>
+
+" Copying files to clipboard
+set clipboard=unnamedplus
+nmap <F3> gg"+yG
+
+set t_Co=8
+
---
+++ setup/template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define fori(i, ini, lim) for(int i = int(ini); i < int(lim); i++)
+#define ford(i, ini, lim) for(int i = int(ini); i >= int(lim); i--)
+
+#define debug(x) cout << "> " << #x << " = " << x << endl;
+#define debug_at(arr, at) cout << "> " << #arr << "[" << at << "] = " << arr[at] << endl;
+#define debug_pair(p) cout << "> " << #p << " = (" << p.first << ", " << p.second << ")" << endl;
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+
+	return 0;
+}

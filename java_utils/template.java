afea3eb6a8850d0c6b8a8cb6bf6b8eb155efd3f9
Marlon Mendes <marlonacademy@hotmail.com>
2017-09-02 16:45:40 -0300

    Java IO template

---
+++ java_utils/template.java
@@ -0,0 +1,44 @@
+package template;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+     
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                	final String line = reader.readLine();
+                	if(line == null) {
+                		return null;
+                	}
+                    tokenizer = new StringTokenizer(line);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+        	return Integer.parseInt(next());
+        }
+    }
+}
